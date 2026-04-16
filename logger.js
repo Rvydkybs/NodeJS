e62a5e9614f2d2bc7e50aaa0e4484f43ac1218f3
Rüveyda Kayabaşı <rvyd.kybs@outlook.com>
2026-04-16 16:53:09 +0300

    Delete logger.js

--- logger.js
+++
@@ -1,7 +0,0 @@
-var url="http://mylogger.io/log";
-function log(message) {
-    //send an http request to the server
-    console.log( message);
-}
-module.exports.log = log;
-// module.exports.endPoint = url;
